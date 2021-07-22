323bddb7993a0134689431dc7047d9eace4fe46d
R4nsomW4re <blake.campbell09@gmail.com>
2021-07-21 21:01:26 -0700

    Add functionality

--- text_object.py
+++ text_object.py
@@ -3,3 +3,18 @@ class text_object:
 
     def __init__(self, text: str) -> None:
         self.text = text
+
+    def get_character(self) -> str:
+        return str(self.text[self.cursor_pos])
+
+    def next_character(self) -> None:
+        self.cursor_pos += 1
+
+    def prev_character(self) -> None:
+        self.cursor_pos -= 1
+
+    def set_character(self, pos) -> None:
+        self.cursor_pos = pos
+
+    def reset_character(self) -> None:
+        self.cursor_pos = 0
