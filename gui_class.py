7f6381c31f27d37207a10fc1710f042eda4aba2a
R4nsomW4re <blake.campbell09@gmail.com>
2021-07-23 18:33:28 -0700

    Removed draw_char_from_object

--- gui_class.py
+++ gui_class.py
@@ -10,19 +10,13 @@ class screen:
     def __init__(self):
         curses.curs_set(0)
         self.stdscr.nodelay(1)
+        curses.start_color()
 
     def draw_text(self, x: int, y: int, text, mode = None):
         if not mode:
             self.stdscr.addstr(y, x, text)
         else:
             self.stdscr.addstr(y, x, text, mode)
-
-    def draw_char_from_text_object(self, x: int, y: int, text_object: text_object, mode = None):
-        print(text_object.get_character())
-        if not mode:
-            self.stdscr.addstr(y, x, text_object.get_character())
-        else:
-            self.stdscr.addstr(y, x, text_object.get_character(), mode)
 
     def attron(self, attribute):
         self.stdscr.attron(attribute)
